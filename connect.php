e7d20036beda41009e50be01455c0823f9c6cb03
Wojciech Kaczmarek <101423770+wojtiii14@users.noreply.github.com>
2024-06-29 23:09:11 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,5 @@
 <?php
 
-    // $host = "localhost";
-    // $db_user = "dibdwdsngv_admin";
-    // $db_password = "wifzyq-cijfup-ducTa6";
-    // $db_name = "dibdwdsngv_my_website";
-    // $db_port = "3306";
-
     $host = "localhost";
     $db_user = "root";
     $db_password = "root";
