1d02a783bbd44baaf8b4714480b4acd4d21a461e
Wojciech Kaczmarek <101423770+wojtiii14@users.noreply.github.com>
2025-10-22 10:06:44 +0200

    22.10 Update

--- files/database.sql
+++ files/database.sql
@@ -5,8 +5,8 @@ CREATE TABLE IF NOT EXISTS `clients`
 `Phone Number` TEXT NOT NULL )
 ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
-INSERT INTO `clients` (`ID`, `Name and Surname`, `E-mail`, `Phone Number`) VALUES
-(1, 'Wojciech Kaczmarek', 'wojtak123@gmail.com', '668792084');
+-- INSERT INTO `clients` (`ID`, `Name and Surname`, `E-mail`, `Phone Number`) VALUES
+-- (1, 'Wojciech Kaczmarek', 'wojtak123@gmail.com', '668792084');
 
 ALTER TABLE `clients`
 ADD PRIMARY KEY (`ID`), ADD UNIQUE KEY `ID` (`ID`);
